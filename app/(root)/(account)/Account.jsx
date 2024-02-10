639ca9c3692a18f6367be9c8c44dfcec9e1f5601
Andrei <andreiwork25@gmail.com>
2024-02-10 18:38:07 +0800

    Temporary Navigation
    + Remember to fix cycle error. Importing the SignInContext into the login screen.

--- app/(root)/(account)/Account.jsx
+++ app/(root)/(account)/Account.jsx
@@ -1,17 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableHighlight } from "react-native";
 
 import { StyleSheet } from "react-native";
 import { COLORS, SIZES } from "../../../constants/theme";
+import { useContext } from "react";
 
-const Account = () => {
+import { SignInContext } from "../../index";
+
+export default function Account() {
+  const [isSignedIn, setIsSignedIn] = useContext(SignInContext);
+
   return (
     <View style={styles.container}>
       <Text>Account</Text>
+      <TouchableHighlight onPress={() => setIsSignedIn(false)}>
+        <Text>Logout</Text>
+      </TouchableHighlight>
     </View>
   );
-};
-
-export default Account;
+}
 
 const styles = StyleSheet.create({
   container: {

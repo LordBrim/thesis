483d25bc04339c9f003deda77015f3adc9c3a87c
Andrei <andreiwork25@gmail.com>
2024-02-14 20:16:23 +0800

    tulog na

--- components/home/UpcomingEvents.jsx
+++ components/home/UpcomingEvents.jsx
@@ -1,12 +1,23 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, SafeAreaView, View, Text, FlatList } from "react-native";
 import { SIZES } from "../../constants/theme";
-import EventCardsList from "./EventCardsList";
+import EventCard from "./EventCard";
 
-export default function UpcomingEvents() {
+export default function UpcomingEvents({ toEvent }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Upcoming Blood Donation Events</Text>
-      <EventCardsList />
+      <SafeAreaView>
+        <FlatList
+          data={sampleData}
+          renderItem={({ item }) => (
+            <EventCard toEvent={toEvent} title={item.title} />
+          )}
+          keyExtractor={(item) => item.id}
+          numColumns={1}
+          scrollEnabled={false}
+          contentContainerStyle={styles.container}
+        />
+      </SafeAreaView>
     </View>
   );
 }
@@ -14,9 +25,18 @@ export default function UpcomingEvents() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: SIZES.medium,
   },
   title: {
     fontSize: SIZES.medium,
     fontWeight: "bold",
   },
 });
+
+const sampleData = [
+  { id: 1, title: "Event Card 1" },
+  { id: 2, title: "Event Card 2" },
+  { id: 3, title: "Event Card 3" },
+  { id: 4, title: "Event Card 4" },
+  { id: 5, title: "Event Card 5" },
+];

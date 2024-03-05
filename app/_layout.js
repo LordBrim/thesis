2ee10d25a9edc5693a512dd72b49cc22e9ef3229
Andrei <andreiwork25@gmail.com>
2024-03-06 05:58:19 +0800

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -3,7 +3,7 @@ import { Stack } from "expo-router/stack";
 export default function StackLayout() {
   return (
     <Stack screenOptions={{ headerShadowVisible: false, headerTitle: "" }}>
-      <Stack.Screen name="(auth)/Login" screenOptions={{ headerShown: true }} />
+      <Stack.Screen name="(auth)/Login" />
       <Stack.Screen name="(auth)/Register" />
       <Stack.Screen name="(auth)/ForgotPassword" />
     </Stack>

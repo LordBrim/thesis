1ea4ec18defccccd03b98bb4dec04cc13dd40334
Andrei <andreiwork25@gmail.com>
2024-03-10 10:16:43 +0800

    yeah baby

--- app/(app)/(tabs)/AccountTab.jsx
+++ app/(app)/(tabs)/AccountTab.jsx
@@ -1,34 +1,50 @@
-import { useContext } from "react";
-import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
+// import { useContext } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { COLORS, SIZES } from "../../../constants/theme";
+import { Link } from "expo-router";
 
-import { SignedInContext } from "../../../context/SignedInContext";
+// import { SignedInContext } from "../../../context/SignedInContext";
 
 export default function AccountTab({ navigation }) {
-  const [isSignedIn, setIsSignedIn] = useContext(SignedInContext);
+  // const [isSignedIn, setIsSignedIn] = useContext(SignedInContext);
 
   return (
     <View style={styles.container}>
-      <TouchableHighlight
-        onPress={() => navigation.navigate("DonationHistory")}
-      >
-        <Text>Donation History</Text>
-      </TouchableHighlight>
-      <TouchableHighlight onPress={() => navigation.navigate("Profile")}>
-        <Text>Profile</Text>
-      </TouchableHighlight>
-      <TouchableHighlight onPress={() => navigation.navigate("Settings")}>
-        <Text>Settings</Text>
-      </TouchableHighlight>
-      <TouchableHighlight onPress={() => navigation.navigate("About")}>
-        <Text>About</Text>
-      </TouchableHighlight>
-      <TouchableHighlight onPress={() => navigation.navigate("Help")}>
-        <Text>Help</Text>
-      </TouchableHighlight>
-      <TouchableHighlight>
-        <Text>Logout</Text>
-      </TouchableHighlight>
+      <Link asChild push href="/DonationHistory">
+        <TouchableOpacity>
+          <Text>Donation History</Text>
+        </TouchableOpacity>
+      </Link>
+
+      <Link asChild push href="/Profile">
+        <TouchableOpacity>
+          <Text>Profile</Text>
+        </TouchableOpacity>
+      </Link>
+
+      <Link asChild push href="/Settings">
+        <TouchableOpacity>
+          <Text>Settings</Text>
+        </TouchableOpacity>
+      </Link>
+
+      <Link asChild push href="/About">
+        <TouchableOpacity>
+          <Text>About</Text>
+        </TouchableOpacity>
+      </Link>
+
+      <Link asChild push href="/Help">
+        <TouchableOpacity>
+          <Text>Help</Text>
+        </TouchableOpacity>
+      </Link>
+
+      <Link asChild replace href="/Login">
+        <TouchableOpacity>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }

dbbe66b2fca5d47163c4130eaaf179f33af458dc
Andrei <andreiwork25@gmail.com>
2024-04-29 12:24:49 +0800

    Added UserRoleContext
    + User role context shares the user role to the whole application at the root.
    + Context renders specific components based on set role
    + Refactored firebase config

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { UserRoleContext, UserRoleType } from "context/UserRoleContext";
 import {
   ForgotPassword,
   Login,
@@ -19,43 +20,52 @@ import {
 } from "./screens";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useState } from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { FIREBASE_AUTH } from "firebase.config";
 const Stack = createNativeStackNavigator();
 
 export default function Authentication() {
+  const [role, setRole] = useState<UserRoleType>("client");
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user);
+    });
+  }, []);
+
   return (
-    <Stack.Navigator initialRouteName="Login">
-      {/* Authentication Activities */}
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-      {/* Home Activities */}
-      <Stack.Screen name="(app)/(tabs)" component={HomeTab} />
-      {/* User Level */}
-      <Stack.Screen name="Donate" component={Donate} />
-      <Stack.Screen name="Request" component={Request} />
-      <Stack.Screen name="EventDetails" component={EventDetails} />
-      {/* Staff Level */}
-      <Stack.Screen name="ManageBloodUnits" component={ManageBloodUnits} />
-      <Stack.Screen name="ManageEvents" component={ManageEvents} />
-      <Stack.Screen name="ManageUsers" component={ManageUsers} />
-      <Stack.Screen name="ManageStaff" component={ManageStaff} />
-      {/* Account Screens */}
-      <Stack.Screen name="About" component={About} />
-      <Stack.Screen name="DonationHistory" component={DonationHistory} />
-      <Stack.Screen name="Help" component={Help} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Settings" component={Settings} />
-      {/* QR Code Screens */}
-      <Stack.Screen name="QRScanner" component={QRScanner} />
-    </Stack.Navigator>
+    <UserRoleContext.Provider value={role}>
+      <Stack.Navigator initialRouteName="Login">
+        {/* Authentication Activities */}
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        {/* Home Activities */}
+        <Stack.Screen name="(app)/(tabs)" component={HomeTab} />
+        {/* User Level */}
+        <Stack.Screen name="Donate" component={Donate} />
+        <Stack.Screen name="Request" component={Request} />
+        <Stack.Screen name="EventDetails" component={EventDetails} />
+        {/* Staff Level */}
+        <Stack.Screen name="ManageBloodUnits" component={ManageBloodUnits} />
+        <Stack.Screen name="ManageEvents" component={ManageEvents} />
+        <Stack.Screen name="ManageUsers" component={ManageUsers} />
+        <Stack.Screen name="ManageStaff" component={ManageStaff} />
+        {/* Account Screens */}
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="DonationHistory" component={DonationHistory} />
+        <Stack.Screen name="Help" component={Help} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Settings" component={Settings} />
+        {/* QR Code Screens */}
+        <Stack.Screen name="QRScanner" component={QRScanner} />
+      </Stack.Navigator>
+    </UserRoleContext.Provider>
   );
 }
-
-// import { SignedInContext } from "../context/SignedInContext";
-// const [isSignedIn, setIsSignedIn] = useState(false);
-// <SignedInContext.Provider value={[isSignedIn, setIsSignedIn]}>
-// </SignedInContext.Provider>

9b470bb78d31ab6ffd0f7c8ad96f4b8242821c04
Andrei <andreiwork25@gmail.com>
2024-04-29 21:19:15 +0800

    Update CallToActionBtn.tsx

--- components/common/CallToActionBtn.tsx
+++ components/common/CallToActionBtn.tsx
@@ -1,6 +1,7 @@
 import { Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import { COLORS, SIZES } from "../../constants/theme";
+import { MINOR_COMPONENT_HEIGHT } from "../../constants/measurements";
 
 type ICallToActionBtn = {
   label: string;
@@ -22,6 +23,7 @@ export default function CallToActionBtn({ label, onPress }: ICallToActionBtn) {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
+    height: MINOR_COMPONENT_HEIGHT,
     justifyContent: "center",
     alignItems: "center",
     padding: SIZES.medium,

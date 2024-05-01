6e14dd7a3572eee5b97a0961b614e2c774e22403
Andrei <andreiwork25@gmail.com>
2024-05-01 21:32:29 +0800

    Update LifelineLogo.tsx

--- components/common/LifelineLogo.tsx
+++ components/common/LifelineLogo.tsx
@@ -5,28 +5,43 @@ import { SIZES, COLORS } from "../../constants";
 export default function LifelineLogo() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Lifeline</Text>
-      <Image
-        style={styles.logo}
-        source={require("../../assets/splash/icon.png")}
-      />
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>Lifeline</Text>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/splash/icon.png")}
+        />
+      </View>
+      <Text style={styles.subtitle}>Bloodbank management system</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    gap: 4,
+  },
+  innerContainer: {
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    gap: 10,
+    gap: 14,
     marginTop: 30,
+    width: 230,
   },
   title: {
     fontSize: SIZES.xxxLarge,
     fontWeight: "900",
     textTransform: "uppercase",
     color: COLORS.primary,
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: "500",
+    textTransform: "capitalize",
+    width: 230,
+    textAlign: "center",
   },
   logo: {
     width: 50,

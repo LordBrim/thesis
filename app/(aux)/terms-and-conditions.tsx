412edbf6753c39aba6bdfee8253d21c15bd10140
Andrei <andreiwork25@gmail.com>
2024-05-01 22:12:25 +0800

    Create terms-and-conditions.tsx

---
+++ app/(aux)/terms-and-conditions.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function TermsAndConditionsScreen() {
+  return (
+    <View>
+      <Text>Terms And Conditions Screen</Text>
+    </View>
+  );
+}

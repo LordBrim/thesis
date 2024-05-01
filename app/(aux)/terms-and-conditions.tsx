130b8b0bb8d64dad20fad02b4c08d2b73edf2f5b
Andrei <andreiwork25@gmail.com>
2024-05-01 22:16:16 +0800

    Update terms-and-conditions.tsx

--- app/(aux)/terms-and-conditions.tsx
+++ app/(aux)/terms-and-conditions.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default function TermsAndConditionsScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Terms And Conditions Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+});

5d8dcb38b429f01ae5c65b4c38ced40f70cf233b
Andrei <andreiwork25@gmail.com>
2024-05-02 14:55:19 +0800

    Create Description.tsx

---
+++ components/common/texts/Description.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { COLORS, SIZES } from "../../../constants";
+
+type IDescription = {
+  description: string;
+};
+
+export default function Description({ description }: IDescription) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.description}>{description}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+  },
+  description: {
+    fontSize: SIZES.medium,
+    textAlign: "justify",
+    color: COLORS.gray,
+  },
+});

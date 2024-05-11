1bb6080cd27a6365fd2f4b3ae5af501827571ba6
Andrei <andreiwork25@gmail.com>
2024-05-12 07:57:35 +0800

    Update ChecklistItem.tsx

--- components/home/ChecklistItem.tsx
+++ components/home/ChecklistItem.tsx
@@ -1,16 +1,38 @@
 import { View, Text, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+import { RadioGroup } from "react-native-radio-buttons-group";
+import { RadioButtonProps } from "react-native-paper";
 
 type IChecklistItem = {
   question: string;
 };
 
 export default function ChecklistItem() {
-  const [answer, setAnswer] = useState(null);
+  const radioButtons: RadioButtonProps[] = useMemo(
+    () => [
+      {
+        id: "1", // acts as primary key, should be unique and non-empty string
+        label: "Option 1",
+        value: "option1",
+      },
+      {
+        id: "2",
+        label: "Option 2",
+        value: "option2",
+      },
+    ],
+    []
+  );
+  const [selectedId, setSelectedId] = useState<string | undefined>();
 
   return (
     <View style={styles.container}>
       <Text>Checklist Item</Text>
+      {/* <RadioGroup
+        radioButtons={radioButtons}
+        onPress={setSelectedId}
+        selectedId={selectedId}
+      /> */}
     </View>
   );
 }

89591fa60a07362f88dd8eb550f1a9ef43e49360
Andrei <andreiwork25@gmail.com>
2024-05-21 17:11:31 +0800

    Nilagyan ko ng steps indicator

---
+++ components/common/StepsIndicator.tsx
@@ -0,0 +1,37 @@
+import { View, Text } from "react-native";
+import React from "react";
+import StepIndicator from "react-native-step-indicator";
+
+interface IStepsIndicator {
+  labels: Array<string>;
+}
+
+export default function StepsIndicator({ labels }: IStepsIndicator) {
+  const customStyles = {
+    stepIndicatorSize: 25,
+    currentStepIndicatorSize: 30,
+    separatorStrokeWidth: 2,
+    currentStepStrokeWidth: 3,
+    stepStrokeCurrentColor: "#fe7013",
+    stepStrokeWidth: 3,
+    stepStrokeFinishedColor: "#fe7013",
+    stepStrokeUnFinishedColor: "#aaaaaa",
+    separatorFinishedColor: "#fe7013",
+    separatorUnFinishedColor: "#aaaaaa",
+    stepIndicatorFinishedColor: "#fe7013",
+    stepIndicatorUnFinishedColor: "#ffffff",
+    stepIndicatorCurrentColor: "#ffffff",
+    stepIndicatorLabelFontSize: 13,
+    currentStepIndicatorLabelFontSize: 13,
+    stepIndicatorLabelCurrentColor: "#fe7013",
+    stepIndicatorLabelFinishedColor: "#ffffff",
+    stepIndicatorLabelUnFinishedColor: "#aaaaaa",
+    labelColor: "#999999",
+    labelSize: 13,
+    currentStepLabelColor: "#fe7013",
+  };
+
+  return (
+    <StepIndicator customStyles={customStyles} labels={labels} stepCount={2} />
+  );
+}

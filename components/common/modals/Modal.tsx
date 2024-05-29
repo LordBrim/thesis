2c92e5903f4ea3e7f4428f8275b54301a90c1394
Andrei <andreiwork25@gmail.com>
2024-05-29 14:38:12 +0800

    Create Modal.tsx
    Separated modal from crowded schedule appointment screen.

---
+++ components/common/modals/Modal.tsx
@@ -0,0 +1,93 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  Modal as RNModal,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { COLORS } from "../../../constants";
+
+interface IModal {
+  showModal: boolean;
+  closeModal: () => void;
+  ticketNumber: string;
+}
+
+export default function Modal({ showModal, closeModal, ticketNumber }: IModal) {
+  return (
+    <RNModal
+      animationType="slide"
+      transparent={true}
+      visible={showModal}
+      onRequestClose={closeModal}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={[styles.modalText, styles.modalHeader]}>Success!</Text>
+          <Text style={styles.modalText}>
+            Please show the code included in this message to the hospital staff
+            to confirm your attendance.
+          </Text>
+          <Text style={[styles.modalText, styles.modalCodeText]}>
+            Here's your code:{" "}
+            <Text style={styles.modalCode}>{ticketNumber}</Text>
+          </Text>
+          <TouchableOpacity style={styles.modalButton} onPress={closeModal}>
+            <Text style={styles.modalButtonText}>OK</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </RNModal>
+  );
+}
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 5,
+    alignItems: "center",
+    width: "80%",
+  },
+  modalHeader: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "red",
+    marginBottom: 10,
+  },
+  modalText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  modalSubtext: {
+    fontSize: 16,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  modalCodeText: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: "red",
+  },
+  modalCode: {
+    fontWeight: "bold",
+  },
+  modalButton: {
+    backgroundColor: COLORS.primary,
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  modalButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});

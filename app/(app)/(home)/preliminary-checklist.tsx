f7fa6aba668524ab1f51d1ef7c806d58cd913349
Andrei <andreiwork25@gmail.com>
2024-05-31 19:07:11 +0800

    Update preliminary-checklist.tsx

--- app/(app)/(home)/preliminary-checklist.tsx
+++ app/(app)/(home)/preliminary-checklist.tsx
@@ -1,29 +1,16 @@
-import {
-  Text,
-  SafeAreaView,
-  FlatList,
-  StyleSheet,
-  ScrollView,
-  View,
-  Dimensions,
-} from "react-native";
+import { SafeAreaView, StyleSheet, View, Dimensions } from "react-native";
 import React, { useState } from "react";
-import ChecklistItem from "components/home/ChecklistItem";
 import { COLORS, HORIZONTAL_SCREEN_MARGIN, SIZES } from "../../../constants";
 import CallToActionBtn from "components/common/CallToActionBtn";
 import { router } from "expo-router";
-import Title from "components/common/texts/Title";
-import Description from "components/common/texts/Description";
 import StepsIndicator from "components/common/StepsIndicator";
 import { DonationScreens, checklistQuestions } from "constants/database";
-import SwiperFlatList from "react-native-swiper-flatlist";
 import ScheduleAppointmentScreen from "./schedule-appointment";
 import PreliminaryChecklist from "components/home/PreliminaryChecklist";
 import Carousel from "pinar";
 
 export default function PreliminaryChecklistScreen() {
   let [screenIndex, setScreenIndex] = useState(0);
-  let [autoplay, setAutoplay] = useState(false);
 
   const cancel = () => {
     router.navigate("(app)/(tabs)");
@@ -39,19 +26,6 @@ export default function PreliminaryChecklistScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <StepsIndicator labels={DonationScreens} step={screenIndex} />
-
-      {/* <SwiperFlatList
-        index={screenIndex}
-        showPagination={false}
-        disableGesture={true}
-      >
-        <View style={styles.child}>
-          <PreliminaryChecklist />
-        </View>
-        <View style={styles.child}>
-          <ScheduleAppointmentScreen />
-        </View>
-      </SwiperFlatList> */}
 
       <Carousel
         ref={(carousel) => {

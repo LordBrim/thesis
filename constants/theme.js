852ab7b2eb9c242bee5367bc41cefebe161ddb38
Andrei <andreiwork25@gmail.com>
2024-09-18 10:03:10 +0800

    Update theme.js

--- constants/theme.js
+++ constants/theme.js
@@ -7,6 +7,11 @@ const COLORS = {
 
   slate50: "#f8fafc",
   slate100: "#f1f5f9",
+  slate200: "#e2e8f0",
+  slate300: "#cbd5e1",
+  slate400: "#94a3b8",
+  slate500: "#64748b",
+  slate700: "#334155",
 
   gray: "#83829A",
   gray2: "#C1C0C8",

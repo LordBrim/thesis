6f456cf99fe79f3031d913e9887bc6c0c9172567
Andrei <andreiwork25@gmail.com>
2024-09-19 15:12:56 +0800

    Completed password screen

--- app/(auth)/forgot-password.tsx
+++ app/(auth)/forgot-password.tsx
@@ -1,10 +1,48 @@
-import { View, Text } from "react-native";
+import CallToActionBtn from "components/common/CallToActionBtn";
+import { COLORS, HORIZONTAL_SCREEN_MARGIN } from "../../constants";
+import { View, Text, TextInput, Image } from "react-native";
 import { StyleSheet } from "react-native";
+import { useState } from "react";
+import TextInputWrapper from "components/common/TextInputWrapper";
+import Title from "components/common/texts/Title";
+import Description from "components/common/texts/Description";
 
 export default function ForgotPassword() {
+  const sendPin = () => {
+    //TODO: Send confimation pin to email
+  };
+
+  const [email, setEmail] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Forgot Password Screen</Text>
+      <Image
+        source={require("../../assets/images/forgot-password.png")}
+        style={{
+          width: "80%",
+          height: 200,
+        }}
+      />
+
+      <View style={{ width: "100%" }}>
+        <Title title="Forgot Password" />
+        <Description description="Enter the email you use to sign in." />
+      </View>
+
+      <View style={{ gap: 24, width: "100%" }}>
+        <TextInputWrapper label="Email">
+          <TextInput
+            value={email}
+            placeholder="Enter your email address..."
+            onChangeText={(email) => setEmail(email)}
+            autoCapitalize="none"
+            autoCorrect={true}
+            enablesReturnKeyAutomatically
+          />
+        </TextInputWrapper>
+
+        <CallToActionBtn label="Reset Password" onPress={() => sendPin()} />
+      </View>
     </View>
   );
 }
@@ -14,5 +52,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: COLORS.white,
+    gap: 40,
+    paddingHorizontal: HORIZONTAL_SCREEN_MARGIN,
   },
 });

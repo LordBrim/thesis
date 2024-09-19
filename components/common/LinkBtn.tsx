43b76ba8155a3871d1b0945df43f99390d877701
Andrei <andreiwork25@gmail.com>
2024-09-19 12:26:29 +0800

    Update LinkBtn.tsx

--- components/common/LinkBtn.tsx
+++ components/common/LinkBtn.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-native";
 import React from "react";
 import { type Href, Link } from "expo-router";
+import { COLORS } from "../../constants";
 
 type ILinkBtn = {
   label: string;
@@ -28,5 +29,6 @@ const styles = StyleSheet.create({
   label: {
     fontWeight: "bold",
     textDecorationLine: "underline",
+    color: COLORS.primary,
   },
 });

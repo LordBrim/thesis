b62d333c554bad468f66df8502221f0cc74cbca7
Andrei <andreiwork25@gmail.com>
2024-10-18 03:59:44 +0800

    Update updates-tab.tsx

--- app/(app)/(tabs)/updates-tab.tsx
+++ app/(app)/(tabs)/updates-tab.tsx
@@ -2,12 +2,19 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 import UpdatesGeneral from "../(updates)/updates-general";
 import UpdatesHistory from "../(updates)/updates-history";
 import UpdatesIncentives from "../(updates)/updates-incentives";
+import { COLORS } from "../../../constants";
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function UpdatesTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarIndicatorStyle: {
+          backgroundColor: COLORS.primary,
+        },
+      }}
+    >
       <Tab.Screen name="General" component={UpdatesGeneral} />
       <Tab.Screen name="Donations" component={UpdatesHistory} />
       <Tab.Screen name="Incentives" component={UpdatesIncentives} />

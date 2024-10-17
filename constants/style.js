8d6a306dd4fea87613d60bc025099d2bbfdbb8d3
Andrei <andreiwork25@gmail.com>
2024-10-18 04:25:35 +0800

    Created a global css module
    Makes the design clean and consistent

---
+++ constants/style.js
@@ -0,0 +1,21 @@
+"use strict";
+
+import { StyleSheet } from "react-native";
+
+export const GS = StyleSheet.create({
+  h1: {
+    fontSize: 20,
+    fontWeight: "400",
+    textTransform: "capitalize",
+  },
+  h2: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textTransform: "capitalize",
+  },
+  h3: {
+    fontSize: 14,
+    fontWeight: "bold",
+    textTransform: "capitalize",
+  },
+});

8078a9b598c1bb3bc8083195046db1950312c403
Andrei <andreiwork25@gmail.com>
2024-10-19 03:58:52 +0800

    Should Check Role Login

---
+++ app/(app)/(admin)/(tabs)/_layout.js
@@ -0,0 +1,43 @@
+import { Tabs } from "expo-router";
+
+import { COLORS } from "../../../../constants";
+import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: COLORS.enabled,
+        tabBarInactiveTintColor: COLORS.disabled,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account-tab"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ focused, color, size }) => (
+            <FontAwesome5
+              name={focused ? "user-alt" : "user"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}

75b30cbd01d5e4a9321037286abc7d7438e1d8c6
Andrei <andreiwork25@gmail.com>
2024-10-24 15:16:01 +0800

    Update database.js

--- constants/database.js
+++ constants/database.js
@@ -1,23 +1,3 @@
-export const FAQuestions = [
-  {
-    id: 1,
-    question: "What is the sample question 1?",
-    answer: "This is the sample answer 1.",
-  },
-  {
-    id: 2,
-    question: "What is the sample question 2?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ante non metus interdum sollicitudin. Nullam vitae magna id nulla tincidunt tincidunt in eget arcu. Proin vitae condimentum elit. Fusce at orci neque.",
-  },
-  {
-    id: 3,
-    question: "What is the sample question 3?",
-    answer:
-      "Lorem ipsum fafas cxvv sfas fasj fak waek j sf a as vvsdfasfa  qweqw dasd czx askda ads",
-  },
-];
-
 export const RequestScreens = ["Request\nGuidelines", "File A\nRequest"];
 
 export const checklistQuestions = [

3c08b49cedae618f783f312dafc9227cbc214c4d
Andrei <andreiwork25@gmail.com>
2024-10-24 14:41:59 +0800

    Delete QuestionCard.tsx

--- components/faq/QuestionCard.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
-import React, { useState } from "react";
-import { COLORS, HORIZONTAL_SCREEN_MARGIN } from "../../constants";
-import IconBtn from "components/common/IconButton";
-
-type IQuestionCard = {
-  question: string;
-  answer: string;
-};

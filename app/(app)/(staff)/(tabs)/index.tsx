3de4f14ff2c6f4a15f54aac9ccbadd85b2193268
Andrei <andreiwork25@gmail.com>
2024-10-30 21:58:44 +0800

    Update index.tsx

--- app/(app)/(staff)/(tabs)/index.tsx
+++ app/(app)/(staff)/(tabs)/index.tsx
@@ -1,15 +1,8 @@
 import { StyleSheet, SafeAreaView, ScrollView, Text } from "react-native";
-import React, { useState, useEffect } from "react";
-import { Events, Welcome } from "../../../../components";
-import { GS, HORIZONTAL_SCREEN_MARGIN } from "../../../../constants";
+import React, { useEffect } from "react";
+import { HORIZONTAL_SCREEN_MARGIN } from "../../../../constants";
 import UpcomingAppointments from "components/home/UpcomingAppointments";
-import SingleBtnModal from "components/common/modals/SingleBtnModal";
-import { COLORS, SIZES, SPACES } from "../../../../constants/theme";
-import { router } from "expo-router";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { View } from "react-native";
+import { COLORS, SPACES } from "../../../../constants/theme";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "app/store";
 import { getCurrentUser } from "rtx/slices/user";
@@ -19,9 +12,7 @@ export default function HomeTab() {
   useEffect(() => {
     dispatch(getCurrentUser());
   }, []);
-
   const { user } = useSelector((state: RootState) => state.user);
-
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView

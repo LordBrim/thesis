02ae4e9aeb38c77eeed981794b012fb1be22a08c
Andrei <andreiwork25@gmail.com>
2024-11-02 15:25:28 +0800

    Added incentives

--- app/(app)/(admin)/(home)/manage-incentives.tsx
+++ app/(app)/(admin)/(home)/manage-incentives.tsx
@@ -1,9 +1,20 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import React, { useEffect } from "react";
-import { COLORS } from "../../../../constants";
+import { COLORS, GS, HORIZONTAL_SCREEN_MARGIN } from "../../../../constants";
 import { useNavigation } from "expo-router";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "app/store";
+import { FontAwesome6, Fontisto } from "@expo/vector-icons";
 
 export default function ManageIncentives() {
+  const { user } = useSelector((state: RootState) => state.user);
+  const { hospitals } = useSelector((state: RootState) => state.hospitals);
+  const incentives = hospitals.find(
+    (section) => section.name === user.hospitalName
+  ).incentives;
+  const hospital = hospitals.find(
+    (section) => section.name === user.hospitalName
+  );
   const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({
@@ -15,9 +26,72 @@ export default function ManageIncentives() {
       headerTitleAlign: "center",
     });
   }, []);
+  const size = 40;
+  const data = Array.from(
+    { length: incentives.incentivesNo },
+    (_, i) => i + 1
+  ).map((number) => {
+    const items = incentives.data.filter((item) => item.incentiveNo === number);
+    const uniqueIncentives = [...new Set(items.map((item) => item.incentive))];
+    return {
+      incentiveNo: number,
+      incentive: uniqueIncentives.length > 0 ? uniqueIncentives.join("") : null,
+    };
+  });
   return (
     <View style={styles.container}>
-      <Text>Manage Incentives</Text>
+      <Text style={[GS.h3, styles.title]}>{user.hospitalName} Incentives</Text>
+      <View style={{ flexDirection: "row" }}>
+        <Text>{hospital.incentives.incentivesNo}</Text>
+        <Text>{incentives.repeatable ? "true" : "false"}</Text>
+        <FlatList
+          data={data}
+          renderItem={({ item, index }) => (
+            <View
+              style={{
+                justifyContent: "flex-start",
+                alignItems: "center",
+                width: 60,
+                gap: 4,
+              }}
+            >
+              <Text>{index + 1}</Text>
+              <Fontisto
+                name="blood"
+                size={size}
+                color={index + 1 <= 8 ? COLORS.primary : COLORS.grayMid}
+              />
+              {item.incentive && (
+                <Text style={{ fontWeight: "bold" }}>{item.incentive}</Text>
+              )}
+            </View>
+          )}
+          keyExtractor={(item, index) => {
+            return index.toString();
+          }}
+          numColumns={Math.round(data.length / 2)}
+          contentContainerStyle={{
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 16,
+            // borderWidth: 1,
+          }}
+          columnWrapperStyle={{}}
+        />
+      </View>
+      <View
+        style={{
+          // borderWidth: 1,
+          flexDirection: "row",
+          gap: 12,
+          alignItems: "center",
+          padding: HORIZONTAL_SCREEN_MARGIN,
+        }}
+      >
+        <FontAwesome6 name="circle-info" size={24} color={COLORS.primary} />
+        <Text style={{ flex: 1 }}>{incentives.info}</Text>
+      </View>
     </View>
   );
 }
@@ -26,7 +100,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.background,
-    justifyContent: "center",
-    alignItems: "center",
+    gap: 16,
+  },
+  scrollview: {},
+  title: {
+    minWidth: "100%",
+    paddingHorizontal: HORIZONTAL_SCREEN_MARGIN,
   },
 });

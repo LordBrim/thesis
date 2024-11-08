d95f32d4bea585166006ea1ca6e43c78e1d27bac
Andrei <andreiwork25@gmail.com>
2024-11-08 17:43:10 +0800

    Admin creation complete

--- app/(app)/(super)/(home)/manage-admins.tsx
+++ app/(app)/(super)/(home)/manage-admins.tsx
@@ -1,184 +1,181 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  Pressable,
-  SectionList,
-} from "react-native";
-import React, { useEffect } from "react";
-import { COLORS, GS, HORIZONTAL_SCREEN_MARGIN } from "../../../../constants";
-import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "app/store";
-import {
-  deleteQuestion,
-  deleteQuestionInFirebase,
-  getFAQs,
-} from "rtx/slices/faq";
 import IconBtn from "components/common/IconButton";
-import { router, useNavigation } from "expo-router";
+import { HORIZONTAL_SCREEN_MARGIN } from "constants/measurements";
+import { GS } from "constants/style";
+import { COLORS } from "constants/theme";
+import { router, useNavigation, useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { FlatList, Pressable, ScrollView } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  disableAdmins,
+  disableAdminsInFirebase,
+  getAllAdmins,
+} from "rtx/slices/admins";
 
 export default function ManageAdmins() {
-  const { faqs } = useSelector((state: RootState) => state.faqs);
+  const { hospitals } = useSelector((state: RootState) => state.hospitals);
   const dispatch = useDispatch<AppDispatch>();
-  const navigation = useNavigation();
-
   useEffect(() => {
-    dispatch(getFAQs());
+    dispatch(getAllAdmins());
   }, []);
-
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: "Admins",
+      headerTintColor: "#000000",
+      headerTitleStyle: {
+        fontSize: 16,
+      },
+      headerTitleAlign: "center",
+    });
+  }, []);
+  const router = useRouter();
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
         <IconBtn
           icon="plus"
           size={18}
-          onPress={() => router.push("(app)/(admin)/(home)/manage-faq-create")}
+          onPress={() =>
+            router.push("/(app)/(super)/(home)/manage-admins-create")
+          }
         />
       ),
     });
   }, [navigation]);
-
-  const DATA = [
-    {
-      title: "Main dishes",
-      data: [{ name: "Pizza", type: "Hello" }],
-    },
-  ];
-
   return (
-    <View style={styles.container}>
-      <View style={styles.panels}>
-        {/* <FlatList
-          data={faqs}
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        overScrollMode="never"
+        persistentScrollbar={true}
+        style={styles.scrollview}
+      >
+        <FlatList
+          data={hospitals}
           renderItem={({ item }) => (
-            <QuestionPanel title={item.title} questions={item.questions} />
+            <>
+              <Text style={[GS.h2, styles.title]}>{item.name}</Text>
+              <AdminsPanel hospitalName={item.name} />
+            </>
           )}
-          keyExtractor={(item) => item.title}
-          overScrollMode="never"
-          scrollEnabled={true}
-          persistentScrollbar={true}
-          contentContainerStyle={{ gap: 16 }}
-        /> */}
-        {/* <SectionList
-          sections={faqs}
           keyExtractor={(item, index) => {
             return index.toString();
           }}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.question}</Text>
-              <Text>{item.answer}</Text>
-            </View>
-          )}
-          renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
-        /> */}
-      </View>
-    </View>
+          contentContainerStyle={{ gap: 24, paddingBottom: 16 }}
+          scrollEnabled={false}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-type IQuestionPanel = {
-  title: string;
-  questions: Array<{ question: string; answer: string }>;
-};
-
-export function QuestionPanel({ title, questions }: IQuestionPanel) {
-  return (
-    <View style={panel.container}>
-      <Text style={[GS.h3, panel.title]}>{title}</Text>
-      <FlatList
-        contentContainerStyle={(panel.container, { gap: 16 })}
-        data={questions}
-        renderItem={({ item }) => (
-          <QuestionCard
-            title={title}
-            question={item.question}
-            answer={item.answer}
-          />
-        )}
-        keyExtractor={(item) => item.question}
-      />
-    </View>
+function AdminsPanel({ hospitalName }) {
+  const { admins } = useSelector((state: RootState) => state.admins);
+  const hospitalAdmins = admins.filter(
+    (adminsMember) => adminsMember.hospitalName === hospitalName
   );
-}
 
-type IQuestionCard = {
-  title: string;
-  question: string;
-  answer: string;
-};
-
-export function QuestionCard({ title, question, answer }: IQuestionCard) {
-  const handleUpdate = (title, question, answer) => {
-    router.push({
-      pathname: "(app)/(admin)/(home)/manage-faq-update",
-      params: {
-        title: title.toString(),
-        question: question.toString(),
-        answer: answer.toString(),
-      },
-    });
-  };
-
-  const dispatch = useDispatch();
-
-  const handleDelete = (title, deletedQuestion) => {
-    dispatch(
-      deleteQuestion({
-        title: title,
-        deletedQuestion: deletedQuestion,
-      })
-    );
-    deleteQuestionInFirebase(title, deletedQuestion);
-  };
-
+  console.log(hospitalName);
+  console.log(hospitalAdmins);
   return (
     <>
-      <Pressable style={card.qContainer} android_ripple={{ radius: 250 }}>
-        <Text style={card.question}>{question}</Text>
-        <IconBtn
-          icon="pencil"
-          size={18}
-          onPress={() => handleUpdate(title, question, answer)}
-        />
-        <IconBtn
-          icon="trash"
-          size={18}
-          onPress={() => handleDelete(title, { question, answer })}
-          color="red"
-        />
-      </Pressable>
-      <View style={card.aContainer}>
-        <Text style={card.answer}>{answer}</Text>
-      </View>
+      {hospitalAdmins.length > 0 ? (
+        hospitalAdmins.map((item, index) => (
+          <AdminsCard
+            key={index}
+            disabled={item.disabled}
+            uuid={item.uuid}
+            displayName={item.displayName}
+            email={item.email}
+          />
+        ))
+      ) : (
+        <View
+          style={{
+            paddingHorizontal: HORIZONTAL_SCREEN_MARGIN,
+          }}
+        >
+          <Text>There are no admins created.</Text>
+        </View>
+      )}
     </>
+  );
+}
+
+type IAdminsCard = {
+  uuid: string;
+  disabled: boolean;
+  displayName: string;
+  email: string;
+};
+
+function AdminsCard({ uuid, disabled, displayName, email }: IAdminsCard) {
+  const dispatch = useDispatch();
+  const handleDisable = (uuid) => {
+    dispatch(
+      disableAdmins({
+        uuid: uuid,
+        disabled: !disabled,
+      })
+    );
+    disableAdminsInFirebase(uuid, !disabled);
+  };
+  return (
+    <Pressable style={card.qContainer} android_ripple={{ radius: 250 }}>
+      <View style={card.aContainer}>
+        <Text style={card.answer}>
+          UUID:
+          <Text style={{ fontWeight: "normal" }}> {uuid}</Text>
+        </Text>
+        <Text style={card.answer}>
+          Display Name:
+          <Text style={{ fontWeight: "normal" }}> {displayName}</Text>
+        </Text>
+        <Text style={card.answer}>
+          Disabled:
+          <Text style={{ fontWeight: "normal" }}>
+            {" "}
+            {disabled ? "true" : "false"}
+          </Text>
+        </Text>
+        <Text style={card.answer}>
+          Email:<Text style={{ fontWeight: "normal" }}> {email}</Text>
+        </Text>
+      </View>
+      <View>
+        {disabled ? (
+          <IconBtn
+            icon="circle-minus"
+            size={18}
+            color="gray"
+            onPress={() => handleDisable(uuid)}
+          />
+        ) : (
+          <IconBtn
+            icon="circle-minus"
+            size={18}
+            color="red"
+            onPress={() => handleDisable(uuid)}
+          />
+        )}
+      </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: COLORS.background,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  panels: {
-    gap: 20,
-  },
-});
-
-const panel = StyleSheet.create({
-  container: {
     flex: 1,
-    width: "100%",
-    borderColor: COLORS.slate100,
+  },
+  scrollview: {
+    gap: 20,
+    paddingTop: HORIZONTAL_SCREEN_MARGIN,
   },
   title: {
-    flex: 1,
-    minWidth: "100%",
     paddingHorizontal: HORIZONTAL_SCREEN_MARGIN,
-    paddingVertical: 8,
   },
 });
 
@@ -188,25 +185,17 @@ const card = StyleSheet.create({
     minHeight: 35,
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: HORIZONTAL_SCREEN_MARGIN,
-    padding: 2,
-  },
-  question: {
-    flex: 1,
-    fontWeight: "bold",
+    alignItems: "flex-start",
   },
   aContainer: {
-    width: "100%",
+    width: "85%",
     minHeight: 35,
-    flexDirection: "row",
-    alignItems: "center",
     paddingHorizontal: HORIZONTAL_SCREEN_MARGIN,
-    paddingTop: 8,
-    paddingBottom: 16,
+    padding: 8,
   },
   answer: {
-    flex: 1,
+    width: "100%",
     flexDirection: "row",
+    fontWeight: "bold",
   },
 });

751f73a3f231f269f0ecf43c67a7a363f7c1fc46
Angelo Munar <91932024+geloNaru@users.noreply.github.com>
2024-11-16 21:45:55 +0800

    Update LifelineLogo component to use dynamic image sizing based on screen width

--- components/common/LifelineLogo.tsx
+++ components/common/LifelineLogo.tsx
@@ -1,12 +1,16 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import React from "react";
 import { SIZES, COLORS } from "../../constants";
 
 export default function LifelineLogo() {
+  const screenWidth = Dimensions.get("window").width;
+  const imageWidth = screenWidth * 0.7;
+  const imageHeight = imageWidth * 0.2;
+
   return (
     <View style={styles.container}>
       <Image
-        style={{ height: 70, width: "75%" }}
+        style={{ height: imageHeight, width: imageWidth }}
         source={require("../../assets/images/lifeline_logo_text.png")}
       />
     </View>
